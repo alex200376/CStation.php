5446a6324e70be353f12a5c29de2041089f7ed8e
alex200376 <anthorytsang@gmail.com>
2023-03-31 11:14:10 +0800

    modified:   product_detail.php

--- product_detail.php
+++ product_detail.php
@@ -74,10 +74,11 @@
         </div>
     </header>
     <main id="main" style="margin-top: 30px;">
+    <a class="back" href="product.php">Back</a>
         <div class="card-wrapper" style="box-shadow: 12px 4px 73px -15px rgba(99, 99, 99, 0.75);
     -webkit-box-shadow: 12px 4px 73px -15px rgba(99, 99, 99, 0.75);
     -moz-box-shadow: 12px 4px 73px -15px rgba(99, 99, 99, 0.75);">
-         <a class="back" href="product.php">Back</a>
+        
             <div class="cardxd">
                 <?php
                 // Connect to the database
