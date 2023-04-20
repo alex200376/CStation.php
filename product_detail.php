58268be39ea439fcf21acda5cfad1e4bd85440f3
alex200376 <anthorytsang@gmail.com>
2023-04-20 20:49:15 +0800

    modified:   product_detail.php

--- product_detail.php
+++ product_detail.php
@@ -143,8 +143,9 @@
                 // Close the database connection
                 mysqli_close($conn);
                 ?>
-
+            
             </div>
+            <div id="disqus_thread" style="margin-top: 30px;"></div>
         </div>
 
 
@@ -325,8 +326,25 @@
     <script async src="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.js"></script>
 
     <script src="assets/js/main.js"></script>
-
+    <script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://cstation-1.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <script id="dsq-count-scr" src="//cstation-1.disqus.com/count.js" async></script>
 </body>
 
 </html>
